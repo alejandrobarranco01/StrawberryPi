ebe3152ed1cfd4bfae131b9c2db4501e79667f1f
Alejandro <alejandrobarranco01@gmail.com>
2024-12-05 20:11:50 -0700

    DHT11 Documentation Addded

--- dhthw.c
+++ dhthw.c
@@ -5,58 +5,79 @@
 
 #include "dhthw.h"
 
-// Declare dht11_dat as a global variable
-int dht11_dat[5] = {0, 0, 0, 0, 0};
+int dht11_data[5] = {0, 0, 0, 0, 0};
 
-// Initialize DHT11 sensor (send start signal)
-void init_dht11(void)
+/**
+ * This function initalizes the sensor according to DHT11 Humidity &
+ * Temperature Sensor Data Sheet (Section 5.2)
+ * https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf
+ */
+void initializeDHT11()
 {
     pinMode(DHT11_PIN, OUTPUT);
-    digitalWrite(DHT11_PIN, LOW); // Pull pin down for 18 milliseconds
+
+    // Send a low status for 18 microseconds
+    digitalWrite(DHT11_PIN, LOW);
     delay(18);
-    digitalWrite(DHT11_PIN, HIGH); // Then pull it up for 40 microseconds
+
+    // Send a high status for 40 microseconds
+    digitalWrite(DHT11_PIN, HIGH);
     delayMicroseconds(40);
-    pinMode(DHT11_PIN, INPUT); // Set pin to input mode to read data
+
+    // Get it ready to receive input
+    pinMode(DHT11_PIN, INPUT);
 }
 
-// Parse the DHT11 data by shifting the bits into the dht11_dat array
-void parse_dht11_data(uint8_t *bit, uint8_t microseconds)
+/**
+ * This function just determines if a pulse is a 0 or 1 based on the pulse timing.
+ * It stores the value and moves onto the next bit.
+ */
+void parseData(uint8_t *bit, uint8_t microseconds)
 {
-    // printf("DAT array before: %s\n", dht11_dat);
-    dht11_dat[*bit / 8] <<= 1; // Divide every 8 bits for correct processing and shift by one to make room for other bits
-    // printf("Bit: %d\n", *bit);
-    // printf("microseconds: %d\n", microseconds);
-    if (microseconds > 16) // If the microseconds is large, it’s a 1 bit
+    // Shift left to make space for the new bit
+    dht11_data[*bit / 8] <<= 1;
+
+    // If the pulse duration is more than 16 microseconds, it's a 1
+    if (microseconds > 16)
     {
-        dht11_dat[*bit / 8] |= 1; // Set bit to a 1
+        dht11_data[*bit / 8] |= 1;
     }
-    // printf("DAT array after: %s\n", dht11_dat);
+
+    // Move on to the next bit
     (*bit)++;
 }
 
-// Verify if the checksum is correct
-int verify_checksum(void)
+/**
+ * This function makes makes sure the data is valid according to the
+ * DHT11 Humidity & Temperature Sensor Data Sheet, last byte should be the sum
+ * of the previous four
+ */
+int checkChecksum()
 {
-    return (dht11_dat[4] == ((dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF));
+    return (dht11_data[4] == ((dht11_data[0] + dht11_data[1] + dht11_data[2] + dht11_data[3]) & 0xFF));
 }
 
-int *printData(void)
+/**
+ * This function reads the temperature and humidity from the DHT11.
+ */
+int *readDHT11()
 {
     uint8_t laststate = HIGH;
-    uint8_t microseconds = 0;
+    uint8_t microseconds = 0; // Will use this to time the pulse
     uint8_t bit = 0, i;
-    float f; /* fahrenheit */
+    float f;
 
-    dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+    // Erase any previously stored data for a new reading
+    dht11_data[0] = dht11_data[1] = dht11_data[2] = dht11_data[3] = dht11_data[4] = 0;
 
-    init_dht11();
+    initializeDHT11();
 
-    /* detect change and read data */
-    // Runs for 40 bits * 2 Transitions Per Bit + Some Buffer
     for (i = 0; i < ITERATIONS; i++)
     {
 
         microseconds = 0;
+
+        // Wait for the signal received to change state
         while (digitalRead(DHT11_PIN) == laststate)
         {
             microseconds++;
@@ -64,48 +85,28 @@ int *printData(void)
             if (microseconds == 255)
                 break;
         }
-        laststate = digitalRead(DHT11_PIN); // Store previous state of pin
+        laststate = digitalRead(DHT11_PIN);
 
         if (microseconds == 255)
-            break; // Break out if takes longer than 255 microseconds
+            break;
 
-        // Ignore first few transitions and only process the length of the the half of the data (the high part)
+        // Skip first four iterations and make sure we're on an even iteration
         if ((i >= 4) && (i % 2 == 0))
         {
-            parse_dht11_data(&bit, microseconds);
+            // Read each bit individually
+            parseData(&bit, microseconds);
         }
     }
 
-    /*
-     * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
-     * print it out if data is good
-     */
-    if ((bit >= 40) && verify_checksum())
+    // If we finally read all 40 bits and the checksum is good then return the data
+    if ((bit >= 40) && checkChecksum())
     {
-        f = dht11_dat[2] * 9. / 5. + 32;
-        // printf("Humidity = %d.%d %% Temperature = %d.%d *C (%.1f *F)\n",
-        //  dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3], f);
-        // Get the current time
-        time_t rawtime;
-        struct tm *timeinfo;
-        char timeBuffer[80];
-
-        time(&rawtime);                 // Get current time
-        timeinfo = localtime(&rawtime); // Convert to local time
-
-        // Format the time and store it in timeBuffer
-        strftime(timeBuffer, sizeof(timeBuffer), "%Y-%m-%d %H:%M:%S", timeinfo);
-
-        // Print the time and data
-        printf("Data collected at %s\n", timeBuffer);
-
-        // Return the array with data
-        return dht11_dat;
+        f = (dht11_data[2] * (float)(9 / 5)) + 32;
+        return dht11_data;
     }
     else
     {
-        printf("Data not good, skip\n");
-
+        printf("Error reading from DHT11\n");
         return NULL;
     }
 }
--- dhthw.h
+++ dhthw.h
@@ -3,16 +3,14 @@
 
 #include <stdint.h>
 
-// Pin definitions
 #define DHT11_PIN 21
 #define ITERATIONS 85
 
-// Global variable to store DHT11 data
-extern int dht11_dat[5];
+extern int dht11_data[5];
 
-void init_dht11(void);
-void parse_dht11_data(uint8_t *bit, uint8_t microseconds);
-int verify_checksum(void);
-int *printData(void);
+void initializeDHT11();
+void parseData(uint8_t *bit, uint8_t microseconds);
+int checkChecksum();
+int *readDHT11();
 
 #endif // DHTHW_H
